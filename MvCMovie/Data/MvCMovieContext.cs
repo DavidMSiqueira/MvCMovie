638aee93fcd93227da150c99a83a10c803e57579
David Machado <david.machado20@hotmail.com>
2023-02-22 22:51:09 -0300

    Initial commit

---
+++ MvCMovie/Data/MvCMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvCMovie.Models;
+
+namespace MvCMovie.Data
+{
+    public class MvCMovieContext : DbContext
+    {
+        public MvCMovieContext (DbContextOptions<MvCMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvCMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
