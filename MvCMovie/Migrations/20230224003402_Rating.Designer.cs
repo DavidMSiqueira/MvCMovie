817810512a7074aeb5cee150439b4d4fbdbd0c1e
David Machado <david.machado20@hotmail.com>
2023-02-23 23:16:41 -0300

    Finalização de estrutura
    Foi feita a finalização da estrutura básica com validação nos métodos de create e update.

--- MvCMovie/Migrations/20230224003402_Rating.Designer.cs
+++ MvCMovie/Migrations/20230224003402_Rating.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvCMovie.Data;
 
@@ -11,9 +12,10 @@ using MvCMovie.Data;
 namespace MvCMovie.Migrations
 {
     [DbContext(typeof(MvCMovieContext))]
-    partial class MvCMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20230224003402_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
